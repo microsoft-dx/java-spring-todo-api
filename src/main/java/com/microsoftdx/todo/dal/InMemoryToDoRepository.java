950013569858220000930eaa137ff6df3efc18f2
radu-matei <matei.radu94@gmail.com>
2017-04-01 02:52:59 +0300

    Removed for workshop participants to implement

--- src/main/java/com/microsoftdx/todo/dal/InMemoryToDoRepository.java
+++ src/main/java/com/microsoftdx/todo/dal/InMemoryToDoRepository.java
@@ -32,43 +32,21 @@ public class InMemoryToDoRepository implements IToDoRepository {
 
     @Override
     public ToDoItem getToDoItemById(int id) {
-        for(ToDoItem toDoItem : _toDoList)
-            if(toDoItem.getId() == id)
-                return toDoItem;
-
         return null;
     }
 
     @Override
     public void createToDoItem(ToDoItem toDoItem) {
-        toDoItem.setId(this.getNextId());
-        _toDoList.add(toDoItem);
+
     }
 
     @Override
     public void deleteToDoItem(int id) {
-        ToDoItem toDoItemToDelete= null;
 
-        for(ToDoItem t : _toDoList) {
-            if(t.getId() == id)
-                toDoItemToDelete = t;
-        }
-
-        if(toDoItemToDelete != null)
-            _toDoList.remove(toDoItemToDelete);
     }
 
     @Override
     public void updateToDoItem(ToDoItem toDoItem) {
-
-        ToDoItem toDoItemToUpdate = null;
-
-        for(ToDoItem t : _toDoList){
-            if(t.getId() == toDoItem.getId())
-                toDoItemToUpdate = t;
-        }
-
-        if(toDoItemToUpdate != null)
-            toDoItemToUpdate.UpdateToDoItem(toDoItem);
+        
     }
 }
